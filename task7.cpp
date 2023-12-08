bb9bb06979c947eb381ed28288f5cf56962a8647
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2023-12-09 02:28:46 +0500

    Create task7.cpp

---
+++ task7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+cout<<"\t           /\\\n";
+cout<<"\t          /  \\\n";
+cout<<"\t         /    \\\n";
+cout<<"\t        /______\\\n";
+cout<<"\t       |        |\n";
+cout<<"\t       |        |\n";
+cout<<"\t       |        |\n"; 
+return 0;
+}
